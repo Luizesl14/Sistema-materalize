690533124cf5f6136748ac28d9e36eae56a7e960
Luizesl14 <luiz.develop@gmail.com>
2021-07-04 13:03:10 -0300

    Criando entidades

---
+++ src/main/java/com/materalize/entities/EvntoCadastro.java
@@ -0,0 +1,46 @@
+package com.materalize.entities;
+
+public class EvntoCadastro {
+
+    //atributos
+    private String nome;
+    private String email;
+    private  String celular;
+    private String password;
+
+
+    //Getters and Setters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
